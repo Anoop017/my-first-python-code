c86e9c08b6483bb3ff68eef3d8fdc63be957de98
Anoop017 <anoopsnair1123@gmail.com>
2026-01-06 00:01:12 +0530

    Third Commit

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-def second_function(a, b):
-    return a + b
+a = 16
+b = 3
+c = a//b
+print(c)
+
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+print(total, diff, product, division, remainder, floor_division, exponential)
 
 
-print(second_function(5, 8))
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am %s %s. I teach %s' % (first_name, last_name, language)
+print(formated_string)
