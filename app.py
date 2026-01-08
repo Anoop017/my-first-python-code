26dd730d3c334dcebe91940cd08f03ac992a6b5b
Anoop017 <anoopsnair1123@gmail.com>
2026-01-09 02:48:18 +0530

    Fifth Commit

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ total = a + b
 diff = a - b
 
 
-
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
+first_name = 'Anoop'
+last_name = 'Nair'
 language = 'Python'
 formated_string = 'I am %s %s. I teach %s' % (first_name, last_name, language)
 print(formated_string)
